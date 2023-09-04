efaaffe0799b9a6fe0457f205d6a7cd36bf90a83
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2023-09-04 11:56:10 -0300

    Initial commit

---
+++ mod1.R
@@ -0,0 +1,8 @@
+voigt <- RcppFaddeeva::Voigt
+# 1st step ----------------------------------------------------------------
+# Fix spectra window (x)
+# Random peak number - 2 to 15
+# Fix wn step 2 cm-1
+
+
+seed <- 42
